4023efc51e5425bbeb56394a409f3c4633a32d8d
Violet DeMarco <viiiveee@proton.me>
2024-05-05 14:37:42 -0400

    add edit and delete endpoints

--- routes/notes-router.js
+++ routes/notes-router.js
@@ -46,4 +46,54 @@ router.post("/add", (req, res, next) => {
     })
 })
 
+router.delete("/del", (req, res, next) => {
+    let sql = `DELETE FROM notes WHERE (id=?)`
+    let params = [req.body.id]
+    let errors = []
+    if (!req.body.id) {
+        errors.push("Missing required field id")
+    }
+    if (errors.length > 0) {
+        res.status(400).json({"error": errors.join(",")})
+        return
+    }
+    db.run(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({"error": err.message})
+            return
+        }
+        res.json({
+            "message": "success",
+            "id": req.body.id
+        })
+    })
+})
+
+router.patch("/edit", (req, res, next) => {
+    let sql = `UPDATE notes SET text = ? WHERE id = ?`
+    let params = [req.body.text, req.body.id]
+    let errors = []
+    if (!req.body.id) {
+        errors.push("Missing required field id")
+    }
+    if (!req.body.text) {
+        errors.push("Missing required field text")
+    }
+    if (errors.length > 0) {
+        res.status(400).json({"error": errors.join(",")})
+        return
+    }
+    db.run(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({"error": err.message})
+            return
+        }
+        res.json({
+            "message": "success",
+            "id": req.body.id,
+            "text": req.body.text
+        })
+    })
+})
+
 module.exports = router
